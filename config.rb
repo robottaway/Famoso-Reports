44e035ce74090258865a45d823cf952c333e6f54
Robert Ottaway <robottaway@gmail.com>
2012-04-30 22:10:42 -0700

    SASS, user model with basic password functions built in

---
+++ config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "famoso_reports/static/css"
+sass_dir = "sass"
+images_dir = "famoso_reports/static/images"
+javascripts_dir = "famoso_reports/statis/js"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
---
+++ famoso_reports/templates/signin.mak
@@ -0,0 +1,10 @@
+<form action='authenticate' method='POST'>
+<fieldset>
+<legend>Please enter your username and password</legend>
+<ul>
+<li><label for="username">Password</label><input type="text" name="username" /></li>
+<li><label for="password">Password</label><input type="text" name="password" /></li>
+<li><button type="submit">Sign In</button></li>
+</ul>
+</fieldset>
+</form>
